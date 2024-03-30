ce06bd29ed7920ad17f17d7fe8da81866d706ffc
ZipiZipi <veljkoned@gmail.com>
2024-03-30 12:04:57 +0100

    Abstract pickable
    Sheep and Stun classess

---
+++ Assets/_Scripts/Tiles/Stun.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stun : Pickable
+{
+    protected override void OnPickUp(Player player)
+    {
+        // Logika za stavljanje igrača u stanje paralize, ako je potrebno
+    }
+}
