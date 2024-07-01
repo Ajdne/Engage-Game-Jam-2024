7a9db5200db0aed7fdb61f4afb6c28fc328aaa59
Veljko Nedeljkovic <veljkoned@gmail.com>
2024-07-02 00:17:09 +0200

    Audio Manager
    New audio manager

---
+++ Assets/_Scripts/Managers/Audio/Sound.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts.Managers.Audio
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string Name;
+        public AudioClip Clip;
+    }
+}
